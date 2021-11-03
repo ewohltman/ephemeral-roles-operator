888494428bbade31a7d4821cb4b471ea60cd76ae
Eric Wohltman <eric.wohltman@gmail.com>
2021-11-03 12:25:30 -0400

    Clean up CRD

--- src/version.rs
+++ src/version.rs
@@ -5,12 +5,6 @@ use serde::{Deserialize, Serialize};
 /// Ephemeral Roles Version resource spec
 #[derive(CustomResource, Deserialize, Serialize, Clone, Debug, JsonSchema)]
 #[kube(kind = "Version", group = "ephemeral-roles.net", version = "v1")]
-#[kube(status = "VersionStatus")]
 pub struct VersionSpec {
-    version: String,
-}
-
-#[derive(Deserialize, Serialize, Clone, Debug, JsonSchema)]
-pub struct VersionStatus {
-    is_deployed: bool,
+    enabled: bool,
 }
